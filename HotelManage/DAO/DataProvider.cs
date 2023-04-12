3a02f03ff150f371f28ff3edda3d3baa02cb79b8
anphuprox <75016328+anphuprox@users.noreply.github.com>
2023-04-12 11:31:19 +0700

    login khong mat khau va DAO co ban

--- HotelManage/DAO/DataProvider.cs
+++ HotelManage/DAO/DataProvider.cs
@@ -6,7 +6,114 @@ using System.Threading.Tasks;
 
 namespace HotelManage.DAO
 {
-    class DataProvider
+   public class DataProvider
     {
+        private static DataProvider instance;
+
+        public static DataProvider Instance
+        {
+            get { if (instance == null) instance = new DataProvider(); return DataProvider.instance; }
+            private set { DataProvider.instance = value; }
+        }
+        private string conntstr = "Data Source=.\\sqlexpress;Initial Catalog=ONLINEFOOD;Integrated Security=True";//thay doi cho phu hop voi db
+        public DataTable ExcuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(conntstr))
+            {
+
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listpara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listpara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+
+            }
+            return data;
+        }
+
+        public int ExcutenonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(conntstr))
+            {
+
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listpara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listpara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+                data = command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+            return data;
+        }
+        public object ExcuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(conntstr))
+            {
+
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listpara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listpara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+
+                data = command.ExecuteScalar();
+                connection.Close();
+
+            }
+            return data;
+        }
     }
+}
 }

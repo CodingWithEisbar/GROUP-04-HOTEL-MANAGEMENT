8fb3b6ea3d6c4e79868ee54f39facb35d375998e
LamDDT <108422896+LamDDT@users.noreply.github.com>
2023-05-10 09:40:41 +0700

    finish

--- HotelManage/Checkin/Checkin.Designer.cs
+++ HotelManage/Checkin/Checkin.Designer.cs
@@ -49,7 +49,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(593, 39);
+            this.label1.Location = new System.Drawing.Point(598, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(267, 55);
             this.label1.TabIndex = 0;
@@ -59,9 +59,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1197, 103);
+            this.button1.Location = new System.Drawing.Point(1197, 56);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 52);
+            this.button1.Size = new System.Drawing.Size(136, 52);
             this.button1.TabIndex = 1;
             this.button1.Text = "Tìm";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,28 +70,30 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(1197, 179);
+            this.button2.Location = new System.Drawing.Point(1197, 116);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 52);
+            this.button2.Size = new System.Drawing.Size(136, 95);
             this.button2.TabIndex = 2;
             this.button2.Text = "Thanh toán";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(1208, 549);
+            this.button3.Location = new System.Drawing.Point(1208, 620);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(136, 112);
             this.button3.TabIndex = 3;
             this.button3.Text = "Khuyến mãi";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 103);
+            this.label2.Location = new System.Drawing.Point(33, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(139, 36);
             this.label2.TabIndex = 4;
@@ -100,7 +102,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(178, 103);
+            this.textBox1.Location = new System.Drawing.Point(178, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(1003, 44);
             this.textBox1.TabIndex = 5;
@@ -109,37 +111,39 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(39, 169);
+            this.dataGridView1.Location = new System.Drawing.Point(39, 116);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1142, 101);
+            this.dataGridView1.Size = new System.Drawing.Size(1142, 261);
             this.dataGridView1.TabIndex = 6;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(39, 549);
+            this.dataGridView2.Location = new System.Drawing.Point(39, 620);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 82;
             this.dataGridView2.RowTemplate.Height = 33;
-            this.dataGridView2.Size = new System.Drawing.Size(1142, 131);
+            this.dataGridView2.Size = new System.Drawing.Size(1142, 231);
             this.dataGridView2.TabIndex = 7;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(252, 328);
+            this.textBox2.Location = new System.Drawing.Point(241, 383);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(929, 44);
+            this.textBox2.Size = new System.Drawing.Size(940, 44);
             this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(44, 328);
+            this.label3.Location = new System.Drawing.Point(33, 385);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(202, 36);
             this.label3.TabIndex = 9;
@@ -149,28 +153,30 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Times New Roman", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(1208, 328);
+            this.button4.Location = new System.Drawing.Point(1208, 375);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(168, 52);
+            this.button4.Size = new System.Drawing.Size(125, 94);
             this.button4.TabIndex = 8;
-            this.button4.Text = "Hiển thị";
+            this.button4.Text = "Hiển thị KH";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(39, 392);
+            this.dataGridView3.Location = new System.Drawing.Point(39, 443);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 82;
             this.dataGridView3.RowTemplate.Height = 33;
-            this.dataGridView3.Size = new System.Drawing.Size(1142, 101);
+            this.dataGridView3.Size = new System.Drawing.Size(1142, 145);
             this.dataGridView3.TabIndex = 11;
+            this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
             // 
             // Checkin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1389, 708);
+            this.ClientSize = new System.Drawing.Size(1353, 863);
             this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
--- HotelManage/Checkin/Checkin.cs
+++ HotelManage/Checkin/Checkin.cs
@@ -28,11 +28,6 @@ namespace Checkin
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
 
@@ -40,10 +35,160 @@ namespace Checkin
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string inputText = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(inputText))
+            {
+                // Create connection to SQL Server
+                SqlConnection connection = new SqlConnection("Data Source=LAMDDT-LAPTOP;Initial Catalog=QLKHACHSAN;Integrated Security=True");
+
+                // Create command for stored procedure
+                SqlCommand command = new SqlCommand("GetTHONGTINDATPHONGById", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                // Create adapter for result set
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                // Create datatable to hold result set
+                DataTable dataTable = new DataTable();
+
+                // Fill datatable with data from adapter
+                adapter.Fill(dataTable);
+
+                // Bind datatable to datagridview
+                dataGridView1.DataSource = dataTable;
+            }
+            else
+            {
+                string madat = inputText;
+                // Create connection to SQL Server
+                SqlConnection connection = new SqlConnection("Data Source=LAMDDT-LAPTOP;Initial Catalog=QLKHACHSAN;Integrated Security=True");
+
+                // Create command for stored procedure
+                SqlCommand command = new SqlCommand("GetTHONGTINDATPHONGById", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                // Add parameters for stored procedure
+                command.Parameters.AddWithValue("@MADAT", madat);
+
+                // Create adapter for result set
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                // Create datatable to hold result set
+                DataTable dataTable = new DataTable();
+
+                // Fill datatable with data from adapter
+                adapter.Fill(dataTable);
+
+                // Bind datatable to datagridview
+                dataGridView1.DataSource = dataTable;
+            }
+            
             
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox2.Text.Trim();
+            string makh = inputText;
+            // Create connection to SQL Server
+            SqlConnection connection = new SqlConnection("Data Source=LAMDDT-LAPTOP;Initial Catalog=QLKHACHSAN;Integrated Security=True");
+
+            // Create command for stored procedure
+            SqlCommand command = new SqlCommand("usp_GetKhachHangByMAKH", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            // Add parameters for stored procedure
+            command.Parameters.AddWithValue("@MAKH", makh);
+
+            // Create adapter for result set
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            // Create datatable to hold result set
+            DataTable dataTable = new DataTable();
+
+            // Fill datatable with data from adapter
+            adapter.Fill(dataTable);
+
+            // Bind datatable to datagridview
+            dataGridView3.DataSource = dataTable;
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text.Trim();
+            string madat = inputText;
+            // Create connection to SQL Server
+            SqlConnection connection = new SqlConnection("Data Source=LAMDDT-LAPTOP;Initial Catalog=QLKHACHSAN;Integrated Security=True");
+
+            // Create command for stored procedure to update the data
+            SqlCommand commandUpdate = new SqlCommand("usp_UpdateThongTinDatPhong", connection);
+            commandUpdate.CommandType = CommandType.StoredProcedure;
+            commandUpdate.Parameters.AddWithValue("@MADAT", madat);
+
+            // Execute the command to update the data
+            connection.Open();
+            int rowsAffected = commandUpdate.ExecuteNonQuery();
+            connection.Close();
+
+            // Create command for stored procedure to get the updated data
+            SqlCommand commandGetData = new SqlCommand("GetTHONGTINDATPHONGById", connection);
+            commandGetData.CommandType = CommandType.StoredProcedure;
+            commandGetData.Parameters.AddWithValue("@MADAT", madat);
+
+            // Create adapter for result set
+            SqlDataAdapter adapter = new SqlDataAdapter(commandGetData);
+
+            // Create datatable to hold result set
+            DataTable dataTable = new DataTable();
+
+            // Fill datatable with data from adapter
+            adapter.Fill(dataTable);
+
+            // Bind datatable to datagridview
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            // Create connection to SQL Server
+            SqlConnection connection = new SqlConnection("Data Source=LAMDDT-LAPTOP;Initial Catalog=QLKHACHSAN;Integrated Security=True");
+
+            // Create command for stored procedure
+            SqlCommand command = new SqlCommand("usp_GetAllKhuyenMaiTheoPhong", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            
+
+            // Create adapter for result set
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            // Create datatable to hold result set
+            DataTable dataTable = new DataTable();
+
+            // Fill datatable with data from adapter
+            adapter.Fill(dataTable);
+
+            // Bind datatable to datagridview
+            dataGridView2.DataSource = dataTable;
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
